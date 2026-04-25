3dbf65c5ecab4884b21ba8af5975b92f4f1d432d
rxdrigocrn <rodrigocirinocl@gmail.com>
2026-04-25 12:07:32 -0300

    chore: build api url

--- services/api.ts
+++ services/api.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api";
+export const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://backend-blog-efomm.vercel.app/api";
 
 import { useAuthStore } from "@/store/authStore";
 import { toast } from "react-toastify";
